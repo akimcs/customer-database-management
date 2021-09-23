12253b15343565549d7f3683dfae7b0bc3116372
akimcs <19701110+akimcs@users.noreply.github.com>
2021-09-23 16:01:22 -0400

    Standardized cus/apt menu and add screens. Finished fxml ids and methods.

--- src/controller/APTaddController.java
+++ src/controller/APTaddController.java
@@ -1,14 +1,65 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import mainApplication.Main;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class APTaddController implements Initializable {
+
+    @FXML
+    private Label appointmentidText;
+    @FXML
+    private TextField titleText;
+    @FXML
+    private TextField descriptionText;
+    @FXML
+    private TextField locationText;
+    @FXML
+    private ComboBox<?> typeCBText;
+    @FXML
+    private ComboBox<?> StartHrText;
+    @FXML
+    private ComboBox<?> contactCBText;
+    @FXML
+    private DatePicker dateDPText;
+    @FXML
+    private ComboBox<?> StartMinText;
+    @FXML
+    private ComboBox<?> customeridCBText;
+    @FXML
+    private ComboBox<?> useridCBText;
+    @FXML
+    private ComboBox<?> EndHrText;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Main.getStage().setTitle("Add Appointment");
+    }
+
+    @FXML
+    void clickCancelButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSubmitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectedStartHour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectedStartMin(ActionEvent event) {
+
     }
 }
--- src/controller/APTmenuController.java
+++ src/controller/APTmenuController.java
@@ -1,15 +1,80 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import mainApplication.Main;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class APTmenuController implements Initializable {
+
+    @FXML
+    private TableView<?> appointmentTableview;
+    @FXML
+    private TableColumn<?, ?> appIdText;
+    @FXML
+    private TableColumn<?, ?> titleColumn;
+    @FXML
+    private TableColumn<?, ?> descriptionColumn;
+    @FXML
+    private TableColumn<?, ?> locationColumn;
+    @FXML
+    private TableColumn<?, ?> contactColumn;
+    @FXML
+    private TableColumn<?, ?> typeColumn;
+    @FXML
+    private TableColumn<?, ?> startColumn;
+    @FXML
+    private TableColumn<?, ?> endColumn;
+    @FXML
+    private TableColumn<?, ?> cusIdColumn;
+    @FXML
+    private TableColumn<?, ?> userIdColumn;
+    @FXML
+    private ToggleGroup Timespan;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Main.getStage().setTitle("Appointment Menu");
+
+    }
+
+    @FXML
+    void clickAllRadioButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickMonthRadioButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickWeekRadioButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickAddButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickDeleteButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickMainscreenButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickModifyButton(ActionEvent event) {
 
     }
 }
