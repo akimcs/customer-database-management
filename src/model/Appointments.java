d7c9866b61e706893d7d4dfb2691c25eff628931
akim67 <akim67@wgu.edu>
2021-09-23 03:18:43 -0400

    Created Appointment Class and sqlRecord.

---
+++ src/model/Appointments.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class Appointments {
+
+    private int id;
+    private String title;
+    private String description;
+    private String location;
+    private int contact;
+    private String type;
+    private Timestamp date;
+    private Timestamp start;
+    private Timestamp end;
+    private int customerId;
+    private int userId;
+
+
+
+}
