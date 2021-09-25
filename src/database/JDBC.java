1c8faece6d1130431ddf8aed48d4dc0dca35d66f
akimcs <19701110+akimcs@users.noreply.github.com>
2021-09-24 22:01:11 -0400

    Fixed JDBC disconnect conn reference.

--- src/database/JDBC.java
+++ src/database/JDBC.java
@@ -8,19 +8,19 @@ public class JDBC {
 
     // SELECT
     public static ResultSet exQuery(String stmt) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/client_schedule", "sqlUser", "Passw0rd!");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/client_schedule", "sqlUser", "Passw0rd!");
         return conn.createStatement().executeQuery(stmt);
     }
 
     // INSERT, UPDATE, DELETE
     public static void exUpdate(String stmt) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/client_schedule", "sqlUser", "Passw0rd!");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/client_schedule", "sqlUser", "Passw0rd!");
         conn.createStatement().executeUpdate(stmt);
     }
 
     // Prepared Statement
     public static PreparedStatement pStatement(String stmt) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/client_schedule", "sqlUser", "Passw0rd!");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/client_schedule", "sqlUser", "Passw0rd!");
         return conn.prepareStatement(stmt);
     }
 
